ed8c48c5ee715624f77fa5b38ab8f9e369ad5671
prontogas <jacksonfrancodogas@gmail.com>
2025-06-26 19:59:14 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 function gerarCodigo() {
-  const codigo = 'PG-' + Math.floor(1000 + Math.random() * 9000);
-  document.getElementById('codigo').innerText = 'Seu pedido foi gerado: ' + codigo;
+  const codigo = "PG-" + Math.floor(Math.random() * 10000).toString().padStart(4, '0');
+  document.getElementById("codigo").innerText = "Seu pedido foi gerado: " + codigo;
+
+  const numero = "5521975433826"; // seu número para teste
+  const mensagem = `Olá! Gostaria de fazer um pedido. Código: ${codigo}`;
+  const link = `https://wa.me/${numero}?text=${encodeURIComponent(mensagem)}`;
+
+  document.getElementById("whatsappLink").href = link;
 }
